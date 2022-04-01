14022022752ce8cdeb08b90d3c491a398f80b8e6
neuralsrg <jsetup@hotmail.com>
2022-04-01 22:35:52 +0300

    In process

--- FLTK/classes.cxx
+++ FLTK/classes.cxx
@@ -7,8 +7,10 @@
 
 /* Cell */
 
+Array<SellInfo> Cell::field;
+
 Cell::Cell(int x, int y, int w, int h, short ot) :
-	Fl_Button(x, y, w, h, ""), objType(ot) {}
+	Fl_Button(x, y, w, h, ""), objType(ot), btnParams{x, y, w, h} {}
 
 void Cell::setCallback(Scene *sc)
 {
@@ -20,10 +22,59 @@ void Cell::callback_cell(Fl_Widget *w, void *u)
 	((Cell *)w)->click(w, u);
 }
 
+void Cell::fillField(std::shared_ptr<Cell> ptr_to_me)
+{
+	field.insertAtEnd(ptr_to_me);
+}
+
+void Cell::boomBubblePretenders(int n)
+{
+	for (int sellIndex = 0; sellIndex < field.getLength(); ++sellIndex) {
+		if (field[sellIndex].pretendList.getLength() <= 1)
+			continue;
+		int pretIndex = 0;
+		while (pretIndex < field[sellIndex].pretendList.getLength()) {
+			if (field[sellIndex].pretendList[pretIndex]->objType != OBJ_BUBBLE) {
+				++pretIndex;
+				continue;
+			}
+			std::shared_ptr<Cell> bubble =
+				field[sellIndex].pretendList[pretIndex];
+			int bubblesCurrentIndex = bubble->getCurrentIndex(sellIndex, n);
+			field[bubblesCurrentIndex].owner =
+				std::make_shared<EmptyCell>(bubble->btnParams);
+			field[sellIndex].pretendList.remove(pretIndex);
+		}
+	}
+}
+
+void Cell::moveObjects(int n)
+{
+	int sellIndex = 0;
+	while (sellIndex < field.getLength()) {
+		if (field[sellIndex].owner->objType != OBJ_EMPTY ||
+				field[sellIndex].pretendList.getLength() != 1) {
+			++sellIndex;
+			continue;
+		}
+		std::shared_ptr<Cell> obj =
+			field[sellIndex].pretendList[0];
+		int objCurrentIndex = obj->getCurrentIndex(sellIndex, n);
+		field[objCurrentIndex].owner =
+			std::make_shared<EmptyCell>(obj->btnParams);
+		field[sellIndex].owner = obj;
+		field[sellIndex].pretendList.erase();
+		sellIndex = 0;
+	}
+}
+
 /* EmptyCell */
 
 EmptyCell::EmptyCell(int x, int y, int w, int h) :
 	Cell(x, y, w, h, OBJ_EMPTY) {}
+
+EmptyCell::EmptyCell(int btnParams[4]) :
+	Cell(btnParams[0], btnParams[1], btnParams[2], btnParams[3], OBJ_EMPTY) {}
 
 void EmptyCell::click(Fl_Widget *w, void *u)
 {
@@ -32,17 +83,52 @@ void EmptyCell::click(Fl_Widget *w, void *u)
 
 /* RoundObj */
 
-Array<int> RoundObj::takenLocations = Array<int>();
-
 RoundObj::RoundObj(int x, int y, int w, int h, short obj) :
-	Cell(x, y, w, h, obj), nextLocation(-1), newDirection(-1) {}
+	Cell(x, y, w, h, obj), direction(-1) {}
 
-void RoundObj::chooseDirection(int index, int n)
+/*
+RoundObj::RoundObj(int btnParams[4], short obj) :
+	Cell(btnParams[0], btnParams[1], btnParams[2], btnParams[3], obj), 
+	direction(-1) {}
+*/
+
+int RoundObj::getCurrentIndex(int newIndex, int n) const
 {
-	short row = index / n,
-		  column = index % n,
-		  newDirection = std::rand() % 8;
-	switch(newDirection) {
+	short row = newIndex / n, column = newIndex % n;
+	switch(direction) {
+		case TOP_LEFT:
+		case LEFT:
+		case BOTTOM_LEFT:
+			++column;
+			break;
+		case TOP_RIGHT:
+		case RIGHT:
+		case BOTTOM_RIGHT:
+			--column;
+			break;
+	}
+	switch(direction) {
+		case TOP_LEFT:
+		case TOP:
+		case TOP_RIGHT:
+			--row;
+			break;
+		case BOTTOM_LEFT:
+		case BOTTOM:
+		case BOTTOM_RIGHT:
+			++row;
+			break;
+	}
+	assert(row >= 0 && column > 0 && "getCurrentIndex computed wrong index!");
+	return row * n + column;
+}
+
+void RoundObj::chooseLocation(int index, int n, std::shared_ptr<Cell> ptr_to_me)
+{
+	direction = std::rand() % 8;
+	short row = index / n, column = index % n;
+
+	switch(direction) {
 		case TOP_LEFT:
 		case LEFT:
 		case BOTTOM_LEFT:
@@ -54,7 +140,7 @@ void RoundObj::chooseDirection(int index, int n)
 			++column;
 			break;
 	}
-	switch(newDirection) {
+	switch(direction) {
 		case TOP_LEFT:
 		case TOP:
 		case TOP_RIGHT:
@@ -70,20 +156,13 @@ void RoundObj::chooseDirection(int index, int n)
 		row = index / n;
 		column = index % n;
 	}
-	nextLocation = row * n + column;
-	assert(nextLocation >= 0 && "Bad location set!");
-	takenLocations.insertAtEnd(nextLocation);
-}
-
-void RoundObj::checkAbilityToMove(Array<RoundObj *> *arr)
-{
-	assert(takenLocations.count(nextLocation) > 0 && "Location not found!");
-	if (takenLocations.count(nextLocation) == 1)
+	int nextLocation = row * n + column;
+	if (nextLocation == index)
 		return;
-	arr->insertAtEnd(this);
+	assert(nextLocation >= 0 && "Bad location set!");
+	field[nextLocation].pretendList.insertAtEnd(ptr_to_me);
+	std::cout << "New pretend attached to " << nextLocation << std::endl;
 }
-
-void RoundObj::doneMoving() { takenLocations.erase(); }
 
 /* Bubble */
 
@@ -93,9 +172,4 @@ Bubble::Bubble(int x, int y, int w, int h) :
 void Bubble::click(Fl_Widget *w, void *u)
 {
 	std::cout << "This is Bubble\n";
-}
-
-short Bubble::fixIssue()
-{
-	return MOVING_DELETED;
 }
--- FLTK/classes.h
+++ FLTK/classes.h
@@ -9,7 +9,20 @@
 #include <FL/Fl_Button.H>
 #include <iostream>
 
+class Cell;
+class RoundObj;
 class Scene;
+
+class SellInfo
+{
+public:
+
+	std::shared_ptr<Cell> owner;
+	Array<std::shared_ptr<Cell>> pretendList;
+
+	SellInfo() : owner(), pretendList() {}
+	SellInfo(std::shared_ptr<Cell> o) : owner(o), pretendList() {}
+};
 
 class Cell : public Fl_Button
 {
@@ -18,30 +31,38 @@ protected:
 	short objType;
 
 	Cell(int, int, int, int, short);
-	virtual ~Cell() {}
 
 	virtual void click(Fl_Widget *, void *) = 0;
 	void setCallback(Scene *);
 
-	virtual void chooseDirection(int, int) = 0;
-	//virtual int move() = 0;
-
 public:
 
+	int btnParams[4];
+	virtual int getCurrentIndex(int, int) const = 0;
+
 	static void callback_cell(Fl_Widget *w, void *u);
+	static Array<SellInfo> field;
+
+	void fillField(std::shared_ptr<Cell>);
+	virtual void chooseLocation(int, int, std::shared_ptr<Cell>) = 0;
+	static void boomBubblePretenders(int);
+
+	virtual ~Cell() {}
 };
+
 
 class EmptyCell : public Cell
 {
 protected:
 
 	void click(Fl_Widget *, void *) override;
-	void chooseDirection(int, int) override {}
-	//int move() override { return MOVING_OK; }
 
 public:
 	
 	EmptyCell(int, int, int, int);
+	EmptyCell(int[4]);
+	int getCurrentIndex(int, int) const override { return -1; }
+	void chooseLocation(int, int, std::shared_ptr<Cell>) override {}
 	virtual ~EmptyCell() {}
 };
 
@@ -50,18 +71,14 @@ class RoundObj : public Cell
 protected:
 
 	RoundObj(int, int, int, int, short);
-	int nextLocation;
-	short newDirection;
-
-	void chooseDirection(int, int) override;
-	void checkAbilityToMove(Array<RoundObj *> *);
+	//RoundObj(int[4], short);
+	short direction;
 
 public:
 
-	virtual short fixIssue() = 0;
-	void doneMoving();
+	int getCurrentIndex(int, int) const override;
 
-	static Array<int> takenLocations;
+	void chooseLocation(int, int, std::shared_ptr<Cell>) override;
 	virtual ~RoundObj() {}
 };
 
@@ -69,14 +86,19 @@ class Bubble : public RoundObj
 {
 protected:
 	
-	//int move() override;
 	void click(Fl_Widget *, void *) override;
-	short fixIssue() override;
 
 public:
 	
 	Bubble(int, int, int, int);
 	virtual ~Bubble() {}
+};
+
+class Scene
+{
+public:
+
+	Array<Cell> cells;
 };
 
 #endif
